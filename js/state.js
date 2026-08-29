1f4de53c53c539d165192ae9ebbe79323d3810a3
Vanya <38669449+morozvanya1@users.noreply.github.com>
2026-08-29 18:28:15 +0300

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,16 +1,52 @@
 export const state = {
+
   user: null,
+
   daily: [],
+
   planned: [],
+
+  /*
+   * Формат:
+   *
+   * completions[taskId][date] = значение
+   *
+   * Например:
+   *
+   * completions["abc"]["2026-08-29"] = true
+   *
+   * или:
+   *
+   * completions["xyz"]["2026-08-29"] = "125"
+   */
+
   completions: {},
+
+  /*
+   * Голд:
+   *
+   * dailyGold["2026-08-29"] = 125
+   */
+
+  dailyGold: {},
+
   selectedDate: new Date(),
-  calendarMonth: new Date(
-    new Date().getFullYear(),
-    new Date().getMonth(),
-    1
-  ),
+
+  calendarMonth:
+    new Date(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      1
+    ),
+
   calendarSelected: null,
+
   editing: null,
+
   modalType: "daily",
-  drag: null
+
+  drag: null,
+
+  optionalHidden: false
+
 };
